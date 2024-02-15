81e8ffa8e6b4196f89063bb450166d7be70e3cc8
MANIAHMED <abdulmani11111111@gmail.com>
2024-02-15 21:35:45 +0500

    useBalance hook done

---
+++ src/hooks/useBalanceCheck.ts
@@ -0,0 +1,18 @@
+// useBalanceCheck.js
+import { useState } from "react";
+import { formatChainAsNum } from "~/utils";
+
+export const useBalanceCheck = (locking:any, account:any) => {
+  const [balance, setBalance] = useState<any>(0);
+
+  const checkBalance = async () => {
+    try {
+      const response = await locking.balanceOf(account);
+      setBalance(formatChainAsNum(response._hex));
+    } catch (error) {
+      console.error("Error checking balance:", error);
+    }
+  };
+
+  return { balance, checkBalance };
+};
--- src/pages/LockingPage.tsx
+++ src/pages/LockingPage.tsx
@@ -1,165 +1,135 @@
-import { useMetaMask } from "~/hooks/useMetaMask";
-import { useEffect, useState } from "react";
-import { Locking, Wallet } from "@eltk/staking-sdk";
-import { formatChainAsNum } from "~/utils";
-import _votingEscrow from "../abi/votingEscrow.json";
-import {
-  BaseContract,
-  Contract,
-  ContractFactory,
-} from "@ethersproject/contracts";
-import { ethers } from "ethers";
-import { useLocking } from "~/hooks/useLocking";
+  import { useMetaMask } from "~/hooks/useMetaMask";
+  import { useEffect, useState } from "react";
+  import { Locking, Wallet } from "@eltk/staking-sdk";
+  import { formatChainAsNum } from "~/utils";
+  import _votingEscrow from "../abi/votingEscrow.json";
+  import {
+    BaseContract,
+    Contract,
+    ContractFactory,
+  } from "@ethersproject/contracts";
+  import { ethers } from "ethers";
+  import { useLocking } from "~/hooks/useLocking";
+import { useBalanceCheck } from "~/hooks/useBalanceCheck";
+ //voting escrow
+    //0xB4D2c9384af8AEFfC9810feec3914E59E24070eB
 
-export const LockingPage = () => {
-  const { wallet } = useMetaMask();
-  const { locking, amount, setAmount, period, setPeriod, handleLock } = useLocking();
+  export const LockingPage = () => {
+    const { wallet } = useMetaMask();
+    const { locking, amount, setAmount, period, setPeriod, handleLock } = useLocking();
+    const { balance, checkBalance } = useBalanceCheck(locking, wallet.accounts[0]);
 
-  const [balance, setBalance] = useState<any>(0);
-  const [tokenIds, setTokenIds] = useState<any[]>([]); // Initialize with an empty array
-  const [balanceOfNft, setBalanceOfNft] = useState<any>();
-  const [nftTokenId, setNftTokenId] = useState<string>("");
-  const handleNftTokenIdChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setNftTokenId(e.target.value);
-  };
-  //voting escrow
-  //0xB4D2c9384af8AEFfC9810feec3914E59E24070eB
+    // const [balance, setBalance] = useState<any>(0);
+    const [tokenIds, setTokenIds] = useState<any[]>([]);
+    const [balanceOfNft, setBalanceOfNft] = useState<any>();
+    const [nftTokenId, setNftTokenId] = useState<string>("");
+    const handleNftTokenIdChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setNftTokenId(e.target.value);
+    };
+   
 
-  //   useEffect(() => {
+ 
 
-  //   });
+    useEffect(() => {
+      (async () => {
+        const wallet = await Wallet.getSignerFromMetaMask();
+        console.log("wallet", wallet);
+        const locking = new Locking(
+          wallet,
+          "0xB4D2c9384af8AEFfC9810feec3914E59E24070eB"
+        );
+      
+      })();
+    }, []);
 
-  useEffect(() => {
-    (async () => {
-      const wallet = await Wallet.getSignerFromMetaMask();
-      console.log("wallet", wallet);
-      const locking = new Locking(
-        wallet,
-        "0xB4D2c9384af8AEFfC9810feec3914E59E24070eB"
-      );
-      // setLocking(locking);
-      //   const _votingEscrow = new ethers.Contract(
-      //     "0xB4D2c9384af8AEFfC9810feec3914E59E24070eB",
-      //     _votingEscrow?.abi,
-      //     wallet
-      //   );
-      //   try {
-      //     const response = await testAbi.callStatic.locked(1);
-      //   } catch (error) {
-      //     console.log("errrr", error);
-      //   }
-    })();
-  }, []);
+ 
 
-  // const handleLock = async () => {
-  //   // Validate the amount here before calling createLock
-  //   if (!amount || isNaN(parseFloat(amount))) {
-  //     console.error("Please enter a valid amount of tokens.");
-  //     return;
-  //   }
-  //   const amountAsString = amount.toString();
+    const GetTokenIds = async (balance: any) => {
+      const tokenIdArray = [];
 
-  //   const response = await locking.createLock(amountAsString, period);
-  //   console.log("response", response);
-  // };
+      for (let i = 0; i < balance; i++) {
+        try {
+          const response = await locking.getTokenId(wallet.accounts[0], i); // You can adjust the second parameter as needed
+          const formattedResponse = formatChainAsNum(response._hex);
+          tokenIdArray.push(formattedResponse);
+          console.log(`TokenId for iteration ${i}:`, formattedResponse);
+        } catch (error) {
+          console.error(`Error getting tokenId for iteration ${i}:`, error);
+          // Handle errors as needed
+          tokenIdArray.push(null); // Push a placeholder value if an error occurs
+        }
+      }
 
-  const BalanceCheck = async () => {
-  
-    try {
-      const response = await locking.balanceOf(wallet.accounts[0]);
-      setBalance(formatChainAsNum(response._hex));
-      //   setBalance(response);
-    } catch (error) {
-      console.error("Error checking balance:", error);
-    }
-  };
+      console.log("TokenIds:", tokenIdArray);
+      setTokenIds(tokenIdArray);
+    };
 
-  const GetTokenIds = async (balance: any) => {
-    const tokenIdArray = [];
-
-    for (let i = 0; i < balance; i++) {
+    const BalanceOfNft = async () => {
       try {
-        const response = await locking.getTokenId(wallet.accounts[0], i); // You can adjust the second parameter as needed
-        const formattedResponse = formatChainAsNum(response._hex);
-        tokenIdArray.push(formattedResponse);
-        console.log(`TokenId for iteration ${i}:`, formattedResponse);
+        if (!nftTokenId || isNaN(parseInt(nftTokenId))) {
+          console.error("Please enter a valid NFT Token ID.");
+          return;
+        }
+        const response = await locking.balanceOfNFT(parseInt(nftTokenId));
+        setBalanceOfNft(response);
+        console.log("Balance of NFT:", response);
+        // Handle the response as needed
       } catch (error) {
-        console.error(`Error getting tokenId for iteration ${i}:`, error);
-        // Handle errors as needed
-        tokenIdArray.push(null); // Push a placeholder value if an error occurs
+        console.error("Error checking balance of NFT:", error);
       }
-    }
+    };
 
-    console.log("TokenIds:", tokenIdArray);
-    setTokenIds(tokenIdArray);
-  };
-
-  const BalanceOfNft = async () => {
-    try {
-      if (!nftTokenId || isNaN(parseInt(nftTokenId))) {
-        console.error("Please enter a valid NFT Token ID.");
-        return;
-      }
-      const response = await locking.balanceOfNFT(parseInt(nftTokenId));
-      setBalanceOfNft(response);
-      console.log("Balance of NFT:", response);
-      // Handle the response as needed
-    } catch (error) {
-      console.error("Error checking balance of NFT:", error);
-    }
-  };
-
-  return (
-    <div>
-      <div style={{ fontSize: 18, fontWeight: "bold" }}>Locking Hooks</div>
-      <label>
-        Amount of Tokens
-        <input
-          type="number"
-          placeholder="Enter amount"
-          value={amount}
-          onChange={(e) => setAmount(e.target.value)}
-        />
-      </label>
-      <label>
-        Locking Period:
-        <select
-          value={period}
-          onChange={(e) => setPeriod(Number(e.target.value))}
-        >
-          <option value={604800}>1 week</option>
-          <option value={1209600}>2 weeks</option>
-          {/* Add more options as needed */}
-        </select>
-      </label>
-      <button onClick={handleLock}>Lock</button>
+    return (
       <div>
-        <button onClick={BalanceCheck}>CheckBalance</button>
-        <div>{balance}</div>
-      </div>
-      <div>
-        <button onClick={() => GetTokenIds(balance)}>get Tokens Id</button>
+        <div style={{ fontSize: 18, fontWeight: "bold" }}>Locking Hooks</div>
+        <label>
+          Amount of Tokens
+          <input
+            type="number"
+            placeholder="Enter amount"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+          />
+        </label>
+        <label>
+          Locking Period:
+          <select
+            value={period}
+            onChange={(e) => setPeriod(Number(e.target.value))}
+          >
+            <option value={604800}>1 week</option>
+            <option value={1209600}>2 weeks</option>
+            {/* Add more options as needed */}
+          </select>
+        </label>
+        <button onClick={handleLock}>Lock</button>
         <div>
-          {tokenIds.map((tokenId: any, index: any) => (
-            <div
-              key={index}
-            >{`Token ID for iteration ${index}: ${tokenId}`}</div>
-          ))}
+          <button onClick={checkBalance}>CheckBalance</button>
+          <div>{balance}</div>
         </div>
-      </div>
+        <div>
+          <button onClick={() => GetTokenIds(balance)}>get Tokens Id</button>
+          <div>
+            {tokenIds.map((tokenId: any, index: any) => (
+              <div
+                key={index}
+              >{`Token ID for iteration ${index}: ${tokenId}`}</div>
+            ))}
+          </div>
+        </div>
 
-      <div>
-        Balance of Nft
-        <input
-          type="text"
-          placeholder="Enter NFT Token ID"
-          value={nftTokenId}
-          onChange={handleNftTokenIdChange}
-        />
-        <button onClick={BalanceOfNft}>Balance of Nft</button>
-      </div>
+        <div>
+          Balance of Nft
+          <input
+            type="text"
+            placeholder="Enter NFT Token ID"
+            value={nftTokenId}
+            onChange={handleNftTokenIdChange}
+          />
+          <button onClick={BalanceOfNft}>Balance of Nft</button>
+        </div>
 
-      <div>{balanceOfNft?._hex}</div>
-    </div>
-  );
-};
+        <div>{balanceOfNft?._hex}</div>
+      </div>
+    );
+  };
